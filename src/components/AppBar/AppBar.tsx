82696620183e3c72d2ef518dccb3a68f41c830bf
Roman Franko <roman.franko@recommendlabs.com>
2023-05-29 14:45:01 +0300

    config: setup

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import styles from './AppBar.module.css';
+
+interface AppBarProps {}
+
+const AppBar: FC<AppBarProps> = () => (
+  <div className={styles.AppBar}>
+    <input type="text" placeholder="Search..." />
+    <select name="language" id="language">
+      <option value="English">English</option>
+      <option value="Spanish">Spanish</option>
+      <option value="German">German</option>
+    </select>
+    <button>Notification</button>
+    <div className={styles.profileBlock}>
+      <img src="" alt="Profile Image"/>
+      <span>Username</span>
+    </div>
+  </div>
+);
+
+export default AppBar;
---
+++ src/templates/component/TemplateName.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './TemplateName.module.css';
+
+const TemplateName: React.FC = () => {
+    return <div className={styles.container}></div>;
+};
+
+export default TemplateName;
