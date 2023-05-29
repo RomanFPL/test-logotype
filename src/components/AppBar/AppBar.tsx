4e7efa4ac97dce5bd2edafa69f29f053208d99f3
Roman Franko <roman.franko@recommendlabs.com>
2023-05-29 22:01:29 +0300

    config: hook form

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,27 +1,41 @@
 import React, { FC } from "react";
 import styles from "./AppBar.module.css";
 import InputSearch from "../InputSearch/InputSearch";
+import { useForm } from "react-hook-form";
 
 interface AppBarProps {}
 
-const AppBar: FC<AppBarProps> = () => (
-  <div className={styles.AppBar}>
-    <div className={styles.searchBlock}>
-      <InputSearch />
-    </div>
-    <div className={styles.profileBlock}>
-      <select name="language" id="language">
-        <option value="English">English</option>
-        <option value="Spanish">Spanish</option>
-        <option value="German">German</option>
-      </select>
-      <button>Notification</button>
+type FormData = {
+  search: string;
+};
+
+const AppBar: FC<AppBarProps> = () => {
+  const { control } = useForm<FormData>({
+    defaultValues: {
+      search: "",
+    },
+    mode: "onSubmit",
+  });
+
+  return (
+    <div className={styles.AppBar}>
+      <div className={styles.searchBlock}>
+        <InputSearch name={"inputSearch"} control={control} />
+      </div>
       <div className={styles.profileBlock}>
-        <img src="" alt="Profile Image" />
-        <span>Username</span>
+        <select name="language" id="language">
+          <option value="English">English</option>
+          <option value="Spanish">Spanish</option>
+          <option value="German">German</option>
+        </select>
+        <button>Notification</button>
+        <div className={styles.profileBlock}>
+          <img src="" alt="Profile Image" />
+          <span>Username</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AppBar;
--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import styles from "./InputSearch.module.css";
 
 import { ReactComponent as SearchIcon } from "assets/icons/search.svg";
+import { UseControllerProps, useController } from "react-hook-form";
 
-interface InputSearchProps {}
+interface InputSearchProps extends UseControllerProps<any> {}
 
-const InputSearch: React.FC<InputSearchProps> = () => {
+const InputSearch: React.FC<InputSearchProps> = ({
+  name,
+  control,
+  defaultValue,
+  rules,
+  shouldUnregister,
+}) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({ name, control, defaultValue, rules, shouldUnregister });
+  //   TODO add error handling
   return (
     <div className={styles.inputContainer}>
-      <input id="searchInput" type="text" placeholder="Search..." />
-      <label htmlFor="searchInput">
+      <input id={name} type="text" placeholder="Search..." {...field} />
+      <label htmlFor={name}>
         <SearchIcon className={styles.inputIcon} />
       </label>
     </div>
