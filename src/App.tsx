3054e2d48b982bd751f334e1723979d5353c2d25
Roman Franko <roman.franko@recommendlabs.com>
2023-05-29 16:30:56 +0300

    config: redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./App.css";
 import Root from "./Root";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./config/redux/store";
 
 function App() {
   return (
     // TODO add providers here
     <>
-      <BrowserRouter>
-        <Root />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Root />
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
---
+++ src/config/redux/slices/user.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IUser {
+  id: number;
+  firstName: string;
+  secondName: string;
+  level: number;
+}
+
+const initialState: IUser = {} as IUser;
+
+const notifications = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => ({
+      ...state,
+      user: action.payload,
+    }),
+    removeUser: (state, action) => ({
+      ...state,
+      user: initialState,
+    }),
+  },
+});
+
+export default notifications.reducer;
+
+export const { setUser } = notifications.actions;
